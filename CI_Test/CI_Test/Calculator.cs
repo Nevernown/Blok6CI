6aba677e3212f0d378e39394bc64805e77980b5a
Nevernown <ca.boel@student.avans.nl>
2014-11-17 14:20:08 +0100

    F1R5T P05T

---
+++ CI_Test/CI_Test/Calculator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CI_Test
+{
+    public class Calculator
+    {
+        public int Add(int x, int y) 
+        {
+            return x + y;
+        }
+
+        public int Multiply(int x, int y) 
+        {
+            return x * y;
+        }
+    }
+}
---
+++ CI_Test/UnitTestProject1/TestCalc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CI_Test;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class TestCalc
+    {
+        Calculator calc;
+
+        [TestInitialize]
+        public void setup() 
+        {
+            calc = new Calculator();
+        }
+
+        [TestMethod]
+        public void TestAdd()
+        {
+            int x = 5;
+            int y = 7;
+
+            int z = calc.Add(x, y);
+
+            Assert.AreEqual(x + y, z);
+        }
+
+        [TestMethod]
+        public void TestMultiply() 
+        {
+            int x = 5;
+            int y = 7;
+
+            int z = calc.Multiply(x, y);
+
+            Assert.AreEqual(x + y, z);
+        }
+    }
+}
